4d442016c236852fb71a31d93a120ee543f8e177
horace <horace@163.com>
2022-07-27 11:42:34 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author Horace
+ * @Date 2022/7/27 11:37
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
